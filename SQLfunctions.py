707c6b3142f2ebb2d7d67b7e64d774ccbb2020d2
Marianikolaevnav <marianikv1998@gmail.com>
2022-02-21 21:03:03 +0300

    message

--- SQLfunctions.py
+++ SQLfunctions.py
@@ -5,21 +5,21 @@ token = "2073864492:AAH0ksHyEU_9fR-EEVV0_ANe110e0GSWbvM"
 # Функции работы с БД
 
 # добавить пробежку в БД
-def insertRunIntoBD(msg, text):
+def insertRunIntoDB(msg, text):
     with sqlite3.connect('running.db') as con:
         cursor = con.cursor()
         cursor.execute('INSERT INTO runningData (user_id, run) VALUES (?, ?)', (msg.from_user.id, text))
         con.commit()
 
 # добавить награду в БД
-def insertRewardIntoBD(msg, reward):
+def insertRewardIntoDB(msg, reward):
     with sqlite3.connect('running.db') as con:
         cursor = con.cursor()
         cursor.execute('INSERT INTO rewardData (user_id, reward) VALUES (?, ?)',(msg.from_user.id, reward))
         con.commit()
 
 # выбрать все пробежки из БД
-def selectRunsfromBD(msg):
+def selectRunsfromDB(msg):
     with sqlite3.connect('running.db') as con:
         cursor = con.cursor()
         cursor.execute('SELECT run FROM runningData WHERE user_id=={}'.format(msg.from_user.id))
@@ -27,7 +27,7 @@ def selectRunsfromBD(msg):
         return runsList
 
 # выбрать все награды из БД
-def selectRewardsfromBD(msg):
+def selectRewardsfromDB(msg):
     with sqlite3.connect('running.db') as con:
         cursor = con.cursor()
         cursor.execute('SELECT reward FROM rewardData WHERE user_id=={}'.format(msg.from_user.id))
@@ -35,19 +35,19 @@ def selectRewardsfromBD(msg):
         return rewardList
 
 # удаление пробежки из БД
-def deleteRunFromBD(msg):
+def deleteRunFromDB(msg):
     with sqlite3.connect('running.db') as con:
         cursor = con.cursor()
         cursor.execute('DELETE FROM runningData WHERE user_id == ? AND run == ?', (msg.from_user.id, msg.text))
 
 # удаление всех пробежек из БД
-def deleteAllRunsFromBD(msg):
+def deleteAllRunsFromDB(msg):
     with sqlite3.connect('running.db') as con:
         cursor = con.cursor()
         cursor.execute('DELETE FROM runningData WHERE user_id =={}'.format(msg.from_user.id))
 
 # удаление всех наград из БД
-def deleteAllRewardsFromBD(msg):
+def deleteAllRewardsFromDB(msg):
     with sqlite3.connect('running.db') as con:
         cursor = con.cursor()
         cursor.execute('DELETE FROM rewardData WHERE user_id =={}'.format(msg.from_user.id))
