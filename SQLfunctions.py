d041216f820a0aef2f00e67afbacc2a37b52a96d
Marianikolaevnav <marianikv1998@gmail.com>
2022-02-21 21:54:40 +0300

    message

--- SQLfunctions.py
+++ SQLfunctions.py
@@ -4,51 +4,38 @@ token = "2073864492:AAH0ksHyEU_9fR-EEVV0_ANe110e0GSWbvM"
 #--------------------------------
 # Функции работы с БД
 
-# добавить пробежку в БД
-def insertRunIntoDB(msg, text):
+def commit(query, msg, text):
     with sqlite3.connect('running.db') as con:
         cursor = con.cursor()
-        cursor.execute('INSERT INTO runningData (user_id, run) VALUES (?, ?)', (msg.from_user.id, text))
+        cursor.execute(query, (msg.from_user.id, text))
         con.commit()
+
+# добавить пробежку в БД
+def insertRunIntoDB(msg, text):
+    commit('INSERT INTO runningData (user_id, run) VALUES (?, ?)', (msg.from_user.id, text))
 
 # добавить награду в БД
 def insertRewardIntoDB(msg, reward):
-    with sqlite3.connect('running.db') as con:
-        cursor = con.cursor()
-        cursor.execute('INSERT INTO rewardData (user_id, reward) VALUES (?, ?)',(msg.from_user.id, reward))
-        con.commit()
+    commit('INSERT INTO rewardData (user_id, reward) VALUES (?, ?)', (msg.from_user.id, reward))
 
 # выбрать все пробежки из БД
 def selectRunsfromDB(msg):
-    with sqlite3.connect('running.db') as con:
-        cursor = con.cursor()
-        cursor.execute('SELECT run FROM runningData WHERE user_id=={}'.format(msg.from_user.id))
-        runsList = cursor.fetchall()
-        return runsList
+    commit('SELECT run FROM runningData WHERE user_id=={}'.format(msg.from_user.id))
+
 
 # выбрать все награды из БД
 def selectRewardsfromDB(msg):
-    with sqlite3.connect('running.db') as con:
-        cursor = con.cursor()
-        cursor.execute('SELECT reward FROM rewardData WHERE user_id=={}'.format(msg.from_user.id))
-        rewardList = cursor.fetchall()
-        return rewardList
+        commit('SELECT reward FROM rewardData WHERE user_id=={}'.format(msg.from_user.id))
 
 # удаление пробежки из БД
 def deleteRunFromDB(msg):
-    with sqlite3.connect('running.db') as con:
-        cursor = con.cursor()
-        cursor.execute('DELETE FROM runningData WHERE user_id == ? AND run == ?', (msg.from_user.id, msg.text))
+    commit('DELETE FROM runningData WHERE user_id == ? AND run == ?', (msg.from_user.id, msg.text))
 
 # удаление всех пробежек из БД
 def deleteAllRunsFromDB(msg):
-    with sqlite3.connect('running.db') as con:
-        cursor = con.cursor()
-        cursor.execute('DELETE FROM runningData WHERE user_id =={}'.format(msg.from_user.id))
+    commit('DELETE FROM runningData WHERE user_id =={}'.format(msg.from_user.id))
 
 # удаление всех наград из БД
 def deleteAllRewardsFromDB(msg):
-    with sqlite3.connect('running.db') as con:
-        cursor = con.cursor()
-        cursor.execute('DELETE FROM rewardData WHERE user_id =={}'.format(msg.from_user.id))
+    commit('DELETE FROM rewardData WHERE user_id =={}'.format(msg.from_user.id))
 #--------------------------------
